fe6c21c6e6bd00dc680babf620b5efe9de43957e
krol229 <53613309+krol229@users.noreply.github.com>
2023-06-01 11:07:14 +0500

    location1.py

--- hotline_mulyanka/location1.py
+++ hotline_mulyanka/location1.py
@@ -12,13 +12,14 @@ screenmaxy = 0
 screen = pygame.display.set_mode((screenmaxx, screenmaxy))
 myfont = pygame.font.Font('./font/rus8bit.otf', 30)
 location1 = [
-    pygame.image.load('./data/textures/resources/car.png'),
-    pygame.image.load('./data/textures/resources/carmihi.png'),
-    pygame.image.load('./data/textures/resources/Dom.png'),
+    pygame.image.load('./data/textures/resources/car.png'),#0
+    pygame.image.load('./data/textures/resources/carmihi.png'),#1
+    pygame.image.load('./data/textures/resources/dom.png'),#2
     pygame.image.load('./data/textures/resources/map1.png'),#3
-    pygame.image.load('./data/textures/resources/nezahobit.png'),
-    pygame.image.load('./data/textures/resources/rain.png'),
-    pygame.image.load('./data/textures/resources/zabor.png'), 
+    pygame.image.load('./data/textures/resources/nezahobit.png'),#4
+    pygame.image.load('./data/textures/resources/rain.png'),#5
+    pygame.image.load('./data/textures/resources/zabor.png'),#6
+    pygame.image.load('./data/textures/resources/malginhome.png'),#7 
 ]
 misha = [
     #malgin
@@ -39,29 +40,29 @@ mishahodit = [
     pygame.image.load('./data/textures/mihuyek/malgin/maliginlevo.png'),
     pygame.image.load('./data/textures/mihuyek/malgin/maliginpravo.png'),
 ]
-player = pygame.transform.scale(mishahodit[0],(100,100))
+player_size = 1
+#sad = pygame.transform.scale(player_pic, (int(player_size * 1.25), player_size))
+player = pygame.transform.scale(mishahodit[0],(int(player_size), player_size))
 mapa = pygame.transform.scale(location1[3],(1920,1080))
+dom = pygame.transform.scale(location1[2], (200,200))
+dom1 = pygame.transform.scale(location1[2], (200,200))
+dommalgin = pygame.transform.scale((location1[7]),(380,380))
+carmiha = pygame.transform.scale((location1[1]),(300,300))
+zabormihi = pygame.transform.scale((location1[6]),(500, 500))
+car = pygame.transform.scale((location1 [0]), (250, 300))
+nezahodit = pygame.transform.scale((location1[4]), (1000, 400))
 player_x = 1800
 player_y = 980
 player_speed = 7
 player_rect = player.get_rect()
-player_size = 20
 hero_image_number = 0
 player_pos = [player_x, player_y]
 clock = pygame.time.Clock()
-#position = [player_x, player_y]
 schitat = 0
-def rotate(self):
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    rel_x, rel_y = mouse_x - self.x, mouse_y - self.y
-    angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
-    self.image = pygame.transform.rotate(self.original_image, int(angle))
-    self.rect = self.image.get_rect(center=self.position)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
     # Получение текущей позиции курсора мыши
     mouse_pos = pygame.mouse.get_pos()
     keys = pygame.key.get_pressed()
@@ -80,6 +81,9 @@ while running:
         player_y -= player_speed
         hero_image_number += 1
         schitat += 1
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+        sys.exit()
     else:
         hero_image_number = 0
     if schitat >= 0 and schitat <= 19:
@@ -91,14 +95,29 @@ while running:
         schitat = 0
     else:
         hero_image_number = 0
-    #поворот перса
+    screen.blit(mapa, (0, 0))
+    screen.blit(dom, (30,347))
+    screen.blit(dom, (31,642))
+    screen.blit(dom1, (435,491))
+    screen.blit(dommalgin, (1459, 390))
+    screen.blit(carmiha, (1350,887))
+    screen.blit(zabormihi, (1440, 180))
+    screen.blit(car, (1070, 280))
+    screen.blit(nezahodit, (550, 230))
     
+    if player_x <= 1919 and player_y >=1079:
+        player_y = player_y - (player_speed+1)
+    if player_x <= 0 and player_y <= 1079:
+        player_x = player_x + (player_speed+1)
+    if player_x >=1929 and player_y <= 1089:
+        player_x = player_x - (player_speed+1)
+    #поворот перса
     angle = math.atan2(mouse_pos[1] - player_y, mouse_pos[0] - player_x) * 180 / math.pi
     rotated_misha = pygame.transform.rotate(mishahodit[hero_image_number], -angle-90)
-    screen.blit(mapa, (0, 0))
     asd = (player_x - rotated_misha.get_width()/2, player_y - rotated_misha.get_height()/2)
     screen.blit(rotated_misha, asd)
+    
     pygame.display.flip()
-    clock.tick(50)
+    clock.tick(60)
 pygame.quit()
 sys.exit()
